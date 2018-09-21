9fc5e5e40121f42fdea76232858d27445db2859a
egatlin1 <egatlin1@student.alamo.edu>
2018-09-21 13:45:56 -0500

    Added SceneControlls and new test scene
    Added scene controllers allowing scenes to be switched to by name or reloading the current scene
    
    added second test scene ( Test 1 ) with different layout of board
    
    tweaked player size

--- Unity/Mirror/Assets/_Scripts/EventHandler.cs
+++ Unity/Mirror/Assets/_Scripts/EventHandler.cs
@@ -4,12 +4,24 @@ using UnityEngine;
 
 public class EventHandler : MonoBehaviour
 {
+    SceneController sceneController;
 
+    [Tooltip("Name of the next scene to load after this level is won")] public string nextScene;
+
+    public void Awake()
+    {
+        sceneController = FindObjectOfType<SceneController>();
+        if (!sceneController)
+            Debug.LogWarning("No SceneController found");
+    }
 
     // Update is called once per frame
     void Update()
     {
         if (WinPlatform.s_ActivatedWinPlatforms == WinPlatform.s_NumberOfWinPlatforms)
+        {
             Debug.Log("You win!!!!!!");
+            sceneController.loadScene(nextScene);
+        }
     }
 }
