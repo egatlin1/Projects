9d7285bcc7e19c0fa02b7f35cf3ddc3052c140c2
egatlin1 <egatlin1@student.alamo.edu>
2018-09-25 13:26:58 -0500

    Added the SassBot
    SassBot is a little helper bit of text at the bottom... Well, he helps sometimes
    
    Can hold mulitple strings and pics a saying at random each time the level is laoded.

---
+++ Unity/Mirror/Assets/_Scripts/SassBot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SassBot : MonoBehaviour
+{
+    public Text text;
+    public string[] sassyText;
+
+    // Use this for initialization
+    void Awake ( )
+    {
+        if (!text)
+            Debug.LogError("SassBot needs a Text Element ASAP!");
+
+
+        if (sassyText.Length == 0)
+        { }
+        else if (sassyText.Length == 1)
+            text.text = sassyText[0];
+        else
+        {
+            text.text = sassyText[Random.Range(0, sassyText.Length)];
+        }
+    }
+
+}
