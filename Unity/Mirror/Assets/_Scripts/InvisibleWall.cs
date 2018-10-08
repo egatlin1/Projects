37de826173592b2cf0a2b18c903a3aa1190d519f
egatlin1 <egatlin1@student.alamo.edu>
2018-10-07 22:33:12 -0500

    Added more levels

---
+++ Unity/Mirror/Assets/_Scripts/InvisibleWall.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvisibleWall : MonoBehaviour
+{
+   SpriteRenderer sprite;
+
+   void Awake ( )
+   {
+      sprite = GetComponent<SpriteRenderer>();
+      sprite.enabled = false;
+   }
+
+
+   private void OnCollisionEnter2D ( Collision2D other )
+   {
+      sprite.enabled = true;
+   }
+}
