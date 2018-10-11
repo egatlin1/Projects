d18a82ed5bd3806e5d515a3e6e6bf035b588b155
egatlin1 <egatlin1@student.alamo.edu>
2018-10-11 11:32:34 -0500

    VERY rough draft for ball and camera connection
    Still very buggy and will be rewritten when all bugs are found and a better clearer way becomes evident
    
    Camera and ball still don't always line up, camera fallows when it shouldn't and ball can turn horizontally when it shouldn't

---
+++ Unity/Golf/Assets/_Scripts/CameraLookAt.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLookAt : MonoBehaviour
+{
+
+   [SerializeField]
+   private Transform target;
+
+   [SerializeField]
+   private Vector3 offsetPosition;
+
+   [SerializeField]
+   private bool lookAt = true;
+
+   
+   public bool canFollow = true;
+
+
+   private RotationController ball;
+
+   private void Awake ( )
+   {
+      ball = FindObjectOfType<RotationController>();
+
+      ball.canTurn = canFollow;
+   }
+
+
+   private void Update ( )
+   {
+      Refresh();
+
+
+
+      if (Input.GetKeyDown(KeyCode.LeftShift) && canFollow)
+      {
+         canFollow = false;
+         ball.canTurn = false;
+      }
+
+   }
+
+   public void Refresh ( )
+   {
+      if (canFollow)
+      {
+         transform.position = target.TransformPoint(offsetPosition);
+      }
+
+      // compute rotation
+      if (lookAt)
+      {
+         transform.LookAt(target);
+      }
+      else
+      {
+         transform.rotation = target.rotation;
+      }
+   }
+}
---
+++ Unity/Golf/Assets/_Scripts/RotationController.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationController : MonoBehaviour
+{
+   public float rotationSpeed = 30f;
+   public float moveSpeed = 2000f;
+   public bool canTurn = true;
+
+   private bool hasBeenHit = false;
+   private Vector3 startingPos;
+
+   private ConstantForce force;
+   private LineRenderer directionLine;
+   private CameraLookAt m_camera;
+   private Rigidbody m_rigidbody;
+
+   private void Awake ( )
+   {
+      force = GetComponent<ConstantForce>();
+      directionLine = GetComponent<LineRenderer>();
+      m_camera = FindObjectOfType<CameraLookAt>();
+      m_rigidbody = GetComponent<Rigidbody>();
+
+      startingPos = transform.position;
+
+      force.relativeForce = new Vector3(0f, 0f, moveSpeed);
+      force.enabled = false;
+
+      m_rigidbody.constraints = RigidbodyConstraints.FreezePosition;
+
+   }
+
+   private void ResetBall ( )
+   {
+      transform.rotation = Quaternion.Euler( Vector3.zero);
+      directionLine.enabled = true;
+      canTurn = true;
+      hasBeenHit = false;
+      m_camera.canFollow = true;
+      startingPos = transform.position;
+   }
+
+
+   private void Update ( )
+   {
+
+      if ( Input.GetKeyDown(KeyCode.Space) && !canTurn)
+      {
+         m_rigidbody.constraints = RigidbodyConstraints.None;
+         force.enabled = true;
+         directionLine.enabled = false;
+         hasBeenHit = true;
+      }
+
+      //Debug.Log(rigidbody.velocity);
+
+   }
+
+   void FixedUpdate ( )
+   {
+
+      float h = Input.GetAxis("Horizontal");
+      float v = Input.GetAxis("Vertical");
+
+      if ( canTurn && m_rigidbody.velocity == Vector3.zero)
+      {
+         v = 0f;
+      }
+      else if (!canTurn && m_rigidbody.velocity == Vector3.zero)
+      {
+         h = 0f;
+      }
+
+      transform.Rotate(v * rotationSpeed * Time.deltaTime, h * rotationSpeed * Time.deltaTime, 0);
+
+      force.enabled = false;
+
+      if (m_rigidbody.velocity == Vector3.zero && hasBeenHit && startingPos != transform.position)
+      {
+         Debug.Log("Here");
+         ResetBall();
+      }
+   }
+}
