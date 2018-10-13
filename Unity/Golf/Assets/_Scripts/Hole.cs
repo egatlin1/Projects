31a8a58dcd85dd7792eb67ce8db3fe07a2907093
egatlin1 <egatlin1@student.alamo.edu>
2018-10-13 16:46:04 -0500

    Added a flag to the level prefab

---
+++ Unity/Golf/Assets/_Scripts/Hole.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hole : MonoBehaviour
+{
+
+
+   private void OnTriggerEnter ( Collider other )
+   {
+      if (other.tag == "GolfBall")
+         Debug.Log("in the hole!!!!!");
+   }
+
+}
