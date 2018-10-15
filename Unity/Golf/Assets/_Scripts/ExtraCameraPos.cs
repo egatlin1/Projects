511e1019727dde50dd242d0acf7ca90097d62feb
egatlin1 <egatlin1@student.alamo.edu>
2018-10-14 19:39:04 -0500

    Added field camera postions
    Added a way for the main camera to move to follow the golfball after its been hit

---
+++ Unity/Golf/Assets/_Scripts/ExtraCameraPos.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExtraCameraPos : MonoBehaviour
+{
+   [Tooltip("The postion the main camera will be placed when the golfball enters into this collider")]
+   public Transform cameraPosition;
+
+   Camera main;
+
+   private void Awake ( )
+   {
+      main = Camera.main;
+   }
+
+
+   private void OnTriggerEnter ( Collider other )
+   {
+      if ( other.tag == "GolfBall")
+         main.transform.position = cameraPosition.position;
+      
+   }
+
+
+}
