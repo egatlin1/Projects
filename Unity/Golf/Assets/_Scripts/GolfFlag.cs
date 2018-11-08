29e528240c01a4484db4d2297e201388219390fa
egatlin1 <egatlin1@student.alamo.edu>
2018-11-08 16:39:45 -0600

    Re-organized, modified terrain

---
+++ Unity/Golf/Assets/_Scripts/GolfFlag.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GolfFlag : MonoBehaviour
+{
+   Transform flagPosition;
+   public float height;
+   // Use this for initialization
+   void Awake ( )
+   {
+      flagPosition = GameObject.FindGameObjectWithTag("Flag").transform;
+      transform.position = new Vector3(flagPosition.position.x, height, flagPosition.position.z + 15 ); // total cheat I know, but I couldn't figure out how to offset the flag icon properly
+   }
+
+}
