35645393e112e9d4aa932a660ca368c5c22b4f07
egatlin1 <egatlin1@student.alamo.edu>
2018-11-10 13:22:53 -0600

    Added new assets, main menu scene, edited in game UI

---
+++ Unity/Golf/Assets/_Scripts/OrbitAround.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitAround : MonoBehaviour
+{
+   public float speed;
+   public Transform target;
+
+   private void Update ( )
+   {
+      transform.RotateAround(target.transform.position, Vector3.up, speed * Time.deltaTime);
+   }
+}
