9d02041f9bcfe2cb4078db17d77ec178680d50f3
egatlin1 <egatlin1@student.alamo.edu>
2018-11-13 18:00:47 -0600

    Added Icon to UI to keep track of distance from the flag

--- Unity/Golf/Assets/_Scripts/Hole.cs
+++ Unity/Golf/Assets/_Scripts/Hole.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Hole : MonoBehaviour
 {
@@ -9,12 +10,27 @@ public class Hole : MonoBehaviour
 
    public float timeBrforeNextLevel = 5f;
 
+   bool inHole = false;
+   public Text text;
+   GolfBallController golfball;
    SceneController sceneController;
 
    private void Awake ( )
    {
+      golfball = FindObjectOfType<GolfBallController>();
       sceneController = FindObjectOfType<SceneController>();
    }
+
+   private void Update ( )
+   {
+      float dist = Vector3.Magnitude(this.transform.position - golfball.transform.position);
+
+      if (inHole)
+         text.text = "0.00 M";
+      else
+         text.text = dist.ToString("F2") + " M";
+   }
+
 
    private void OnTriggerEnter ( Collider other )
    {
@@ -23,6 +39,7 @@ public class Hole : MonoBehaviour
          Debug.Log("in the hole!!!!!");
          Invoke("RoundOver", timeBrforeNextLevel);
          other.GetComponent<GolfBallController>().canReset = false;
+         inHole = true;
       }
    }
 
