83a7dcda617b17a40725d02471ce123b2ecc9308
egatlin1 <egatlin1@student.alamo.edu>
2018-12-03 09:42:54 -0600

    Fixed golf ball rotation
    Gofl ball is now clamped between 0 degrees(level) and -60 degrees ( upwards angle)

--- Unity/Golf/Assets/_Scripts/GolfBallController.cs
+++ Unity/Golf/Assets/_Scripts/GolfBallController.cs
@@ -5,122 +5,128 @@ using UnityEngine;
 public class GolfBallController : MonoBehaviour
 {
 
-   public float rotationSpeed = 30f;
-   public bool canTurn = true;
-   public bool canReset = true;
-   
-
-   public GameObject UI;
-   public Rigidbody m_rigidbody;
-
-   private bool hasBeenHit = false;
-   private bool isMaxHits = false;
-   public Vector3 startingPos;
-
-   private float slowBallTime = 2f;
-   private float slowBallCountDown = 2f;
-   
-   private LineRenderer directionLine;
-   private CameraLookAt m_camera;
-   private Launcher launcher;
-   private Transform flag;
-   private HitCounter counter;
-
-   private void Awake ( )
-   {
-      directionLine = GetComponent<LineRenderer>();
-      m_camera = FindObjectOfType<CameraLookAt>();
-      m_rigidbody = GetComponent<Rigidbody>();
-      launcher = GetComponent<Launcher>();
-      flag = GameObject.FindGameObjectWithTag("Flag").transform;
-      counter = FindObjectOfType<HitCounter>();
+    public float rotationSpeed = 30f;
+    public bool canTurn = true;
+    public bool canReset = true;
 
 
-      startingPos = transform.position;
+    public GameObject UI;
+    public Rigidbody m_rigidbody;
+
+    private bool hasBeenHit = false;
+    private bool isMaxHits = false;
+    public Vector3 startingPos;
+
+    private float slowBallTime = 2f;
+    private float slowBallCountDown = 2f;
+
+    private LineRenderer directionLine;
+    private CameraLookAt m_camera;
+    private Launcher launcher;
+    private Transform flag;
+    private HitCounter counter;
+
+    private void Awake()
+    {
+        directionLine = GetComponent<LineRenderer>();
+        m_camera = FindObjectOfType<CameraLookAt>();
+        m_rigidbody = GetComponent<Rigidbody>();
+        launcher = GetComponent<Launcher>();
+        flag = GameObject.FindGameObjectWithTag("Flag").transform;
+        counter = FindObjectOfType<HitCounter>();
 
 
-      ResetBall();
-
-   }
-
-   public void RestBallToLastPos ( )
-   {
-      transform.position = startingPos;
-      ResetBall();
-   }
-
-   private void ResetBall ( )
-   {
-      m_rigidbody.constraints = RigidbodyConstraints.FreezeAll;
-
-      // makes the ball always look at the flag when it resets
-      transform.LookAt(flag);
-      Quaternion rotation = transform.rotation;
-      rotation.x = 0.00000f;
-      rotation.z = 0.00000f;
-      transform.rotation = rotation;
+        startingPos = transform.position;
 
 
-      directionLine.enabled = true;
-      canTurn = true;
-      hasBeenHit = false;
-      m_camera.ResetCamera();
-      startingPos = transform.position;
-      UI.SetActive(true);
-   }
+        ResetBall();
+
+    }
+
+    public void RestBallToLastPos()
+    {
+        transform.position = startingPos;
+        ResetBall();
+    }
+
+    private void ResetBall()
+    {
+        m_rigidbody.constraints = RigidbodyConstraints.FreezeAll;
+
+        // makes the ball always look at the flag when it resets
+        transform.LookAt(flag);
+        Quaternion rotation = transform.rotation;
+        rotation.x = 0.00000f;
+        rotation.z = 0.00000f;
+        transform.rotation = rotation;
+
+
+        directionLine.enabled = true;
+        canTurn = true;
+        hasBeenHit = false;
+        m_camera.ResetCamera();
+        startingPos = transform.position;
+        UI.SetActive(true);
+    }
 
 
 
-   void FixedUpdate ( )
-   {
+    void FixedUpdate()
+    {
 
-      float h = Input.GetAxis("Horizontal");
-      float v = Input.GetAxis("Vertical");
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
 
-      if (canTurn && m_rigidbody.velocity == Vector3.zero)
-      {
-         v = 0f;
-      }
-      if (!canTurn && m_rigidbody.velocity == Vector3.zero)
-      {
-         h = 0f;
+        if (canTurn && m_rigidbody.velocity == Vector3.zero)
+        {
+            v = 0f;
+        }
+        if (!canTurn && m_rigidbody.velocity == Vector3.zero)
+        {
+            h = 0f;
+        }
 
-         //Quaternion rotation = transform.rotation;
-         //Vector3 angle = rotation.eulerAngles;
-         //angle.x = Mathf.Clamp(angle.x, -60, 0);
-         //Debug.Log(angle);
-         //transform.rotation = Quaternion.Euler(angle.x, angle.y, angle.z);
-      }
-
-      if ( !isMaxHits)
-         transform.Rotate(v * rotationSpeed * Time.deltaTime, h * rotationSpeed * Time.deltaTime, 0);
+        if (!isMaxHits)
+        {
+            transform.Rotate(v * rotationSpeed * Time.deltaTime, h * rotationSpeed * Time.deltaTime, 0);
 
 
-      if (m_rigidbody.velocity.magnitude < .5f && hasBeenHit && startingPos != transform.position && canReset)
-      {
-         if (slowBallCountDown <= 0)
-         {
-            ResetBall();
-            isMaxHits = counter.MaxHits();
-            slowBallCountDown = slowBallTime;
-         }
-         else
-            slowBallCountDown -= Time.deltaTime;
-      }
-   }
+            // clamps rotation so that you can't rotate backwards
+            // clamps X rotation of the ball between 0 and -60 degrees ( -60 degrees is an upwards angle)
+            Quaternion rotation = transform.rotation;
+            Vector3 angle = rotation.eulerAngles;
+            angle.x = (angle.x > 180) ? angle.x - 360 : angle.x;
+            angle.x = Mathf.Clamp(angle.x, -60, 0);
+            Debug.Log(angle);
+            transform.rotation = Quaternion.Euler(angle);
+        }
 
 
-   public void LaunchBall ( )
-   {
-      if (!canTurn && m_rigidbody.velocity == Vector3.zero && !isMaxHits )
-      {
-         counter.AddHit();
-         m_rigidbody.constraints = RigidbodyConstraints.None;
-         launcher.LaunchGolfBall();
-         directionLine.enabled = false;
-         hasBeenHit = true;
-         UI.SetActive(false);
-      }
-   }
+        if (m_rigidbody.velocity.magnitude < .5f && hasBeenHit && startingPos != transform.position && canReset)
+        {
+            if (slowBallCountDown <= 0)
+            {
+                ResetBall();
+                isMaxHits = counter.MaxHits();
+                slowBallCountDown = slowBallTime;
+            }
+            else
+                slowBallCountDown -= Time.deltaTime;
+        }
+    }
+
+
+    public void LaunchBall()
+    {
+        if (!canTurn && m_rigidbody.velocity == Vector3.zero && !isMaxHits)
+        {
+            counter.AddHit();
+            m_rigidbody.constraints = RigidbodyConstraints.None;
+            launcher.LaunchGolfBall();
+            directionLine.enabled = false;
+            hasBeenHit = true;
+            UI.SetActive(false);
+        }
+    }
 
 }
