8aeebad8e1d845241c84b8e22bd5e97edcfe0767
egatlin1 <egatlin1@student.alamo.edu>
2019-06-13 14:53:34 -0500

    Fixed typos, started set up of main menu screen for Horde

---
+++ Unity/Rain/Assets/_Arcade/_Horde/_Scripts/Horde_MainMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Horde_MainMenu : MonoBehaviour
+{
+
+    private SceneController scene;
+
+    // Start is called before the first frame update
+    void Start ()
+    {
+        scene = FindObjectOfType<SceneController>();
+    }
+
+
+
+    public void LoadScene ( string sceneToLoad )
+    {
+        scene.LoadScene(sceneToLoad);
+    }
+
+
+
+
+    public void Exit ()
+    {
+        // TODO: delete any minigame related singleton objects, possibly save
+        scene.LoadScene("Arcade");
+    }
+
+
+    // Update is called once per frame
+    void Update ()
+    {
+
+    }
+}
